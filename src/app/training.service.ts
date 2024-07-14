0ef6d488eb6ff382aed109183cb8c9e4c21278e7
Swathy Subramanian <swathy.subramanian@gds.ey.com>
2024-07-14 13:00:44 +0000

    components and services added

--- src/app/training.service.ts
+++ src/app/training.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
-
+import { Training } from '../models/Training';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class TrainingService {
-
-  constructor() { }
+  private trainings: Training[] = [];
+  baseUrl: string;
+  token = localStorage.getItem("token");
+  constructor(private http: HttpClient) {
+    this.baseUrl = "http://localhost:5066/api/Training/";
+  }
+  newTraining(training: Training): Observable<Training> {
+    return this.http.post<Training>(this.baseUrl, training);
+  }
+  getAllTrainings(): Observable<Training[]> {
+    return this.http.get<Training[]>(this.baseUrl);
+  }
+  getTraining(trainingId: number): Observable<Training> {
+    return this.http.get<Training>(this.baseUrl + "ById/" + trainingId);
+  }
+  getByTrainer(trainerId: number): Observable<Training[]> {
+    return this.http.get<Training[]>(this.baseUrl + "ByTrainer/" + trainerId);
+  }
+  getByTechnology(techId: number): Observable<Training[]> {
+    return this.http.get<Training[]>(this.baseUrl + "ByTechnology/" + techId);
+  }
+  updateTraining(trainingId: number, training: Training): Observable<Training> {
+    return this.http.put<Training>(this.baseUrl + trainingId, training);
+  }
+  deleteTraining(trainingId: number): Observable<any> {
+    return this.http.delete(this.baseUrl + trainingId);
+  }
 }
--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,12 +1,95 @@
 import { Component } from '@angular/core';
+import { Training } from '../../models/Training';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { TrainingService } from '../training.service';
 
 @Component({
   selector: 'app-training',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule, CommonModule],
   templateUrl: './training.component.html',
   styleUrl: './training.component.css'
 })
 export class TrainingComponent {
-
+  trainings: Training[];
+  training: Training;
+  trnByTrainer: Training[];
+  trnByTechnology: Training[];
+  constructor(private trainingSvc: TrainingService) {
+    this.trainings = [];
+    this.training = new Training(0, 0, 0, new Date(), new Date());
+    this.trnByTrainer = [];
+    this.trnByTechnology = [];
+    this.showAll();
+  }
+  showAll() {
+    this.trainingSvc.getAllTrainings()
+      .subscribe({
+        next: (response: Training[]) => {
+          this.trainings = response;
+        }, error: (err) => alert(err.error)
+      });
+  }
+  showTraining() {
+    this.trainingSvc.getTraining(this.training.trainingId)
+      .subscribe({
+        next: (response: Training) => {
+          this.training = response;
+        }, error: (err) => alert(err.error)
+      });
+  }
+  showTrainingByTrainer() {
+    this.trainingSvc.getByTrainer(this.training.trainerId)
+      .subscribe({
+        next: (response: Training[]) => {
+          this.trainings = response;
+        }, error: (err) => {
+          this.trnByTrainer = [];
+          alert(err.error)
+        }
+      });
+  }
+  showTrainingByTechnology() {
+    this.trainingSvc.getByTechnology(this.training.technologyId)
+      .subscribe({
+        next: (response: Training[]) => {
+          this.trainings = response;
+        }, error: (err) => {
+          this.trnByTechnology = [];
+          alert(err.error)
+        }
+      });
+  }
+  addTraining() {
+    this.trainingSvc.newTraining(this.training)
+      .subscribe({
+        next: (response: Training) => {
+          alert("New training added.");
+          this.training = new Training(0, 0, 0, new Date(), new Date());
+          this.showAll();
+        }, error: (err) => alert(err.error)
+      });
+  }
+  updateTraining() {
+    this.trainingSvc.deleteTraining(this.training.trainingId)
+      .subscribe({
+        next: (response: any) => {
+          alert("Training details updated.");
+          this.training = new Training(0, 0, 0, new Date(), new Date());
+          this.showAll();
+        }, error: (err) => alert(err.error)
+      });
+  }
+  deleteTraining() {
+    this.trainingSvc.deleteTraining(this.training.trainingId)
+      .subscribe({
+        next: (response: any) => {
+          alert("Training deleted.");
+          this.training = new Training(0, 0, 0, new Date(), new Date());
+          this.showAll();
+        }, error: (err) => alert(err.error)
+      });
+  }
 }
